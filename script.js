b60ec9bd038a10efee5090624e6cb2ba6aff6edd
KonevAndrew <konev.as@dclouds.ru>
2022-09-22 10:25:18 +0700

    add js

---
+++ script.js
@@ -0,0 +1,21 @@
+// let num1 = document.getElementById("num1").textContent;
+// let num2 = document.getElementById("num2").textContent;
+// let num3 = Number(document.getElementById("num3").value);
+// let num4 = Number(document.getElementById("num4").value);
+// let num5 = Number(document.getElementById("num5").value);
+// let num6 = Number(document.getElementById("num6").value);
+// let num7 = Number(document.getElementById("num7").value);
+// let num8 = Number(document.getElementById("num8").value);
+// let num9 = Number(document.getElementById("num9").value);
+// let num0 = Number(document.getElementById("num0").value);
+
+
+document.querySelector('.calculator__wrapper').addEventListener('click', function(event) {
+    const num = event.target;
+
+    // let total = document.getElementById('total')
+  
+
+  
+    console.log(num.innerText);
+  });
