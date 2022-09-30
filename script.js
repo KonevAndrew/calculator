532aea98cae743407d9a199f05dad01e2d23c240
KonevAndrew <konev.as@dclouds.ru>
2022-09-30 17:09:11 +0700

    edits js and html

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
-// let num1 = document.getElementById("num1").textContent;
-// let num2 = document.getElementById("num2").textContent;
-// let num3 = Number(document.getElementById("num3").value);
-// let num4 = Number(document.getElementById("num4").value);
-// let num5 = Number(document.getElementById("num5").value);
-// let num6 = Number(document.getElementById("num6").value);
-// let num7 = Number(document.getElementById("num7").value);
-// let num8 = Number(document.getElementById("num8").value);
-// let num9 = Number(document.getElementById("num9").value);
-// let num0 = Number(document.getElementById("num0").value);
+let firstNumber = "";
+let secondNumber = "";
+let sign = "";
+let total = false;
 
+const numberBtn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 
-document.querySelector('.calculator__wrapper').addEventListener('click', function(event) {
-    const num = event.target;
+const signBtn = ["=", "+", "-", "*", "/", "%"];
 
-    // let total = document.getElementById('total')
-  
+const outTotal = document.querySelector(".calculator__total");
 
-  
-    console.log(num.innerText);
+function clearAll() {
+  firstNumber = "";
+  secondNumber = "";
+  sign = "";
+  total = false;
+  outTotal.textContent = 0;
+}
+
+document.querySelector(".js-set-clear").onclick = clearAll;
+
+document.querySelector(".calculator__wrapper").addEventListener("click", function (event) {
+    if (!event.target.classList.contains("calculator__btn")) return;
+    if (event.target.classList.contains("js-set-clear")) return;
+
+    outTotal.textContent = "";
+
+    const key = event.target.value;
+
+    if (numberBtn.includes(key)) {
+      if (secondNumber === "" && sign === "") {
+        firstNumber += key;
+        outTotal.textContent = firstNumber;
+      }
+      else if (firstNumber !== "" && secondNumber !== "" && total ) {
+        secondNumber = key;
+        total = false;
+        outTotal.textContent = secondNumber;
+      } 
+        else {
+        secondNumber += key;
+        outTotal.textContent = secondNumber;
+
+      }
+      return;
+    } 
+    if (signBtn.includes(key) && key !== '=') {
+      sign = key;
+      outTotal.textContent = sign;
+      return;
+    }
+    if (key === '=') {
+      if (secondNumber === "") secondNumber = firstNumber;
+      switch (sign) {
+        case "+":
+          firstNumber = (+firstNumber) + (+secondNumber);
+          break;
+        case "-":
+          firstNumber = firstNumber - secondNumber;
+          break;
+        case "*":
+          firstNumber = firstNumber * secondNumber;
+          break;  
+        case "/":
+          if (secondNumber === '0') {
+            outTotal.textContent = "¯\_(ツ)_/¯";
+            firstNumber - '';
+            secondNumber = '';
+            sign = '';
+            return;
+          }
+          firstNumber = firstNumber / secondNumber;
+          break;
+      }
+      total = true;
+      outTotal.textContent = firstNumber;
+    }
   });
+
